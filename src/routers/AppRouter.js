5ac8cc9b3ae71c4e8ee3e3aa186a029710cee088
Alberto Maciñeiras <albertomaci@gmail.com>
2021-11-19 18:32:35 +0100

    0. Empezando router-dom
    Empezando la app de router-dom

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,10 @@
+import { BrowserRouter as Routes, Route } from "react-router-dom";
+//import { Route } from "react-router";
+
+export default function AppRouter() {
+    return (
+        <Routes>
+            <Route></Route>
+        </Routes>
+    )
+}
